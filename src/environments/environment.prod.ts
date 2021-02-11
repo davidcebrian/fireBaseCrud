d7ff3920c73d6a8ce8d7a6b2331c5fbc29888d3b
David <davidcebriancortes0@gmail.com>
2021-02-11 15:51:23 +0100

    m

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyAyJ21yOxR-YQXeCIv3BFbHPapXcmLIx6I",
+    authDomain: "fir-crud-7fc4f.firebaseapp.com",
+    projectId: "fir-crud-7fc4f",
+    storageBucket: "fir-crud-7fc4f.appspot.com",
+    messagingSenderId: "943058762981",
+    appId: "1:943058762981:web:0fa8e3a0aa782e3f16a80c",
+    measurementId: "G-E3JG1JRCMD"
+  }
 };
